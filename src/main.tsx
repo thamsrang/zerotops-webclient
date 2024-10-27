2147f1565dccf3943f1b61aa20ac137d0fafe387
thamsrang <thamsrang@gmail.com>
2024-10-27 23:13:36 +0530

    Font source added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
-import "@/styles/font-roboto.scss";
 import "@/styles/globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
